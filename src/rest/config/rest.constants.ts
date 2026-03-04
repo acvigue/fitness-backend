31ef2c94a0434e7c9b812175eaa2156f610f4fd6
Aiden <aiden@vigue.me>
2026-03-04 12:21:13 -0500

    fix(build): cors for vercel

--- src/rest/config/rest.constants.ts
+++ src/rest/config/rest.constants.ts
@@ -7,4 +7,5 @@ export const REST_API_RELEASE = '1.0.0';
 export const CORS_ALLOWED_ORIGINS = [
   /^https?:\/\/localhost(:\d+)?$/,
   /^https?:\/\/127\.0\.0\.1(:\d+)?$/,
+  'https://fittime-teal.vercel.app',
 ];
