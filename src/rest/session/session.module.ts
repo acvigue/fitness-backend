6e049a71e8420335395fb22bd08e64122b7f25ca
SaiKanagal <skanagal@purdue.edu>
2026-03-06 12:27:51 -0500

    fix: fixed issue with session module

--- src/rest/session/session.module.ts
+++ src/rest/session/session.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '@/rest/auth/auth.module';
 import { SessionService } from './session.service';
 import { SessionController } from './session.controller';
 import { prisma } from '@/shared/utils';
 
 @Module({
+  imports: [AuthModule],
   providers: [SessionService, { provide: 'PRISMA', useValue: prisma }],
   controllers: [SessionController],
 })
