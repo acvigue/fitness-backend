944af38ffdc532ec662d983e00942d8a13eae05f
Aiden <aiden@vigue.me>
2026-04-23 18:57:44 -0400

    fix(dto): user privacy dto

--- src/rest/user/dto/update-user-profile-privacy.dto.ts
+++ src/rest/user/dto/update-user-profile-privacy.dto.ts
@@ -1,18 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean } from 'class-validator';
 
 export class UpdateUserProfilePrivacyDto {
-  @ApiProperty({ description: "Privacy Setting for Bio's", example: 'false' })
+  @ApiProperty({ description: "Privacy Setting for Bio's", example: false })
+  @IsBoolean()
   privateBio!: boolean;
 
-  @ApiProperty({ description: 'Privacy Setting for Favorite Sports', example: 'false' })
+  @ApiProperty({ description: 'Privacy Setting for Favorite Sports', example: false })
+  @IsBoolean()
   privateSports!: boolean;
 
-  @ApiProperty({ description: 'Privacy Setting for Tournaments', example: 'false' })
+  @ApiProperty({ description: 'Privacy Setting for Tournaments', example: false })
+  @IsBoolean()
   privateTournaments!: boolean;
 
   @ApiProperty({
     description: 'Privacy Setting for Featured Achievements',
-    example: 'false',
+    example: false,
   })
+  @IsBoolean()
   privateAchievements!: boolean;
 }
